8d4f99d866ddff6075942f698f4f4508d9b8f816
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-28 10:07:55 -0700

    Fixed InitialDocumentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""The Main Flask App"""
 
 from models import storage
 from api.v1.views import app_views
@@ -7,6 +8,7 @@ from os import environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def close_storage(error):
